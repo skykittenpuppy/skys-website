c4af7916546843709f164a83a60ea5808f88dc4c
Hannah Hadfield <robotooth13alternative@gmail.com>
2023-11-13 19:02:24 +0000

    modified website to be more pluraly lol

--- Scripts/iframe.js
+++ Scripts/iframe.js
@@ -2,10 +2,14 @@ let header = document.getElementById("Header");
 let footer = document.getElementById("Footer");
 
 addEventListener('resize', () => {
-	header.style.height = header.contentDocument.body.scrollHeight + 'px';
-	footer.style.height = footer.contentDocument.body.scrollHeight + 'px';
-});	
+	header.style.height = header.contentDocument.body.scrollHeight+2 + 'px';
+	footer.style.height = footer.contentDocument.body.scrollHeight+2 + 'px';
+});
 header.addEventListener('load', () => {
-	header.style.height = header.contentDocument.body.scrollHeight + 'px';
-	footer.style.height = footer.contentDocument.body.scrollHeight + 'px';
-});	
+	header.style.height = header.contentDocument.body.scrollHeight+2 + 'px';
+	footer.style.height = footer.contentDocument.body.scrollHeight+2 + 'px';
+});
+footer.addEventListener('load', () => {
+	header.style.height = header.contentDocument.body.scrollHeight+2 + 'px';
+	footer.style.height = footer.contentDocument.body.scrollHeight+2 + 'px';
+});
