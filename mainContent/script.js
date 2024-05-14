fb421e50462a7087a5b0419cbcd1f43329d47465
Hannah Hadfield <robotooth13alternative@gmail.com>
2024-05-14 02:23:23 +0100

    finished startMenu

---
+++ mainContent/script.js
@@ -0,0 +1,14 @@
+const mainContent = document.querySelector('#mainContent')
+const HomeButton  = document.querySelector("#HomeButton");
+const OCsButton  = document.querySelector("#OCsButton");
+const ArtButton  = document.querySelector("#ArtButton");
+
+HomeButton.addEventListener("click", (event) => {
+	document.body.className = "Home";
+})
+OCsButton.addEventListener("click", (event) => {
+	document.body.className = "OCs";
+})
+ArtButton.addEventListener("click", (event) => {
+	document.body.className = "Art";
+})
