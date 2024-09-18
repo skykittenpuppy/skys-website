31c11f24f21c302d9027af407468ec3a4fbaf5ae
Hannah Hadfield <robotooth13alternative@gmail.com>
2024-09-18 05:16:46 +0100

    Added Last Updated info to Project Packs

--- Projects/script.js
+++ Projects/script.js
@@ -3,39 +3,44 @@ var projList = [
 		func: addLink,
 		name: "Sky's Assorted Sweets",
 		desc: "A Minecraft Mod featuring Small to Medium tweaks",
-		link: "https://github.com/skykittenpuppy/Skys-Assorted-Sweets"
+		link: "https://github.com/skykittenpuppy/Skys-Assorted-Sweets",
 	},
 	{	func: addPack,
 		name: "Sky's Better Carpets",
-		desc: "A Minecraft Resource-Pack that makes carpets overhang onto the block below"
+		desc: "A Minecraft Resource-Pack that makes carpets overhang onto the block below",
+		updt: new Date("2024-09-18"),
 	},
 	{	func: addPack,
 		name: "Sky's Better Discs",
-		desc: "A Minecraft Resource-Pack that retextures music discs to have an even-center"
+		desc: "A Minecraft Resource-Pack that retextures music discs to have an even-center",
+		updt: new Date("2024-09-18"),
 	},
 	{	func: addPack,
 		name: "Sky's Custom Logo",
-		desc: "A Minecraft Resource-Pack that replaces the logo on the main menu"
+		desc: "A Minecraft Resource-Pack that replaces the logo on the main menu",
+		updt: new Date("2024-09-18"),
 	},
 	{	func: addPack,
 		name: "Sky's Farmland Sides",
-		desc: "A Minecraft Resource-Pack that gives farmland blocks unique side textures"
+		desc: "A Minecraft Resource-Pack that gives farmland blocks unique side textures",
+		updt: new Date("2024-09-18"),
 	},
 	{	func: addPack,
 		name: "Sky's Softer Cakes",
-		desc: "A Minecraft Resource-Pack that modifies the cake textures to make them look softer"
+		desc: "A Minecraft Resource-Pack that modifies the cake textures to make them look softer",
+		updt: new Date("2024-09-18"),
 	},
 	{
 		func: addLink,
 		name: "PlateUp! MC",
 		desc: "A Minecraft Data-Pack that aims to recreate the game PlateUp!",
-		link: "https://github.com/skykittenpuppy/PlateUp-MC"
+		link: "https://github.com/skykittenpuppy/PlateUp-MC",
 	},
 	{
 		func: addLink,
 		name: "Community Life Series",
 		desc: "A Minecraft Data-Pack that aims to recreate the mechanics of the youtube Life Series SMP",
-		link: "https://github.com/skykittenpuppy/Community-Life-Series-Datapack"
+		link: "https://github.com/skykittenpuppy/Community-Life-Series-Datapack",
 	},
 ];
 
@@ -65,6 +70,11 @@ function addPack(container, packInfo){
 	desc.innerText = packInfo.desc;
 	desc.classList = "projectDesc"
 	container.appendChild(desc)
+
+	let other = document.createElement("p");
+	other.innerText = "Last updated: "+packInfo.updt.toDateString();
+	other.classList = "projectOther"
+	container.appendChild(other)
 }
 
 function addLink(container, linkInfo){
