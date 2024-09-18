361d8ebcd1586bf27ed647e9daf796534cc7bd78
Hannah Hadfield <robotooth13alternative@gmail.com>
2024-09-18 05:06:24 +0100

    Added Projects Page and Resource-Pack download links

---
+++ Projects/script.js
@@ -0,0 +1,86 @@
+var projList = [
+	{
+		func: addLink,
+		name: "Sky's Assorted Sweets",
+		desc: "A Minecraft Mod featuring Small to Medium tweaks",
+		link: "https://github.com/skykittenpuppy/Skys-Assorted-Sweets"
+	},
+	{	func: addPack,
+		name: "Sky's Better Carpets",
+		desc: "A Minecraft Resource-Pack that makes carpets overhang onto the block below"
+	},
+	{	func: addPack,
+		name: "Sky's Better Discs",
+		desc: "A Minecraft Resource-Pack that retextures music discs to have an even-center"
+	},
+	{	func: addPack,
+		name: "Sky's Custom Logo",
+		desc: "A Minecraft Resource-Pack that replaces the logo on the main menu"
+	},
+	{	func: addPack,
+		name: "Sky's Farmland Sides",
+		desc: "A Minecraft Resource-Pack that gives farmland blocks unique side textures"
+	},
+	{	func: addPack,
+		name: "Sky's Softer Cakes",
+		desc: "A Minecraft Resource-Pack that modifies the cake textures to make them look softer"
+	},
+	{
+		func: addLink,
+		name: "PlateUp! MC",
+		desc: "A Minecraft Data-Pack that aims to recreate the game PlateUp!",
+		link: "https://github.com/skykittenpuppy/PlateUp-MC"
+	},
+	{
+		func: addLink,
+		name: "Community Life Series",
+		desc: "A Minecraft Data-Pack that aims to recreate the mechanics of the youtube Life Series SMP",
+		link: "https://github.com/skykittenpuppy/Community-Life-Series-Datapack"
+	},
+];
+
+let mainSection = document.createElement("span");
+document.querySelector("#ProjectsContent").appendChild(mainSection);
+
+for (let proj in projList) {
+	let container = document.createElement("div")
+	projList[proj].func(container, projList[proj])
+	container.classList = "project"
+	mainSection.appendChild(container);
+}
+
+function addPack(container, packInfo){
+	let icon = document.createElement("img");
+	icon.src = "../CDN/Projects/"+packInfo.name+".png";
+	icon.classList = "projectIcon"
+	container.appendChild(icon);
+
+	let link = document.createElement("a");
+	link.innerText = packInfo.name;
+	link.href = "../CDN/Projects/"+packInfo.name+".zip";
+	link.classList = "projectName directDownload"
+	container.appendChild(link);
+
+	let desc = document.createElement("p");
+	desc.innerText = packInfo.desc;
+	desc.classList = "projectDesc"
+	container.appendChild(desc)
+}
+
+function addLink(container, linkInfo){
+	let icon = document.createElement("img");
+	icon.src = "../CDN/Projects/"+linkInfo.name+".png";
+	icon.classList = "projectIcon"
+	container.appendChild(icon);
+
+	let link = document.createElement("a");
+	link.innerText = linkInfo.name
+	link.href = linkInfo.link;
+	link.classList = "projectName externalLink"
+	container.appendChild(link);
+
+	let desc = document.createElement("p");
+	desc.innerText = linkInfo.desc;
+	desc.classList = "projectDesc"
+	container.appendChild(desc)
+}
--- mainContent/script.js
+++ mainContent/script.js
@@ -1,16 +1,18 @@
-const mainContent = document.querySelector('#mainContent')
-const HomeButton  = document.querySelector("#HomeButton");
-const OCsButton  = document.querySelector("#OCsButton");
-const ArtButton  = document.querySelector("#ArtButton");
-
-HomeButton.addEventListener("click", (event) => {
+document.querySelector("#HomeButton")
+		.addEventListener("click", (event) => {
 	document.body.className = "Home";
 })
-OCsButton.addEventListener("click", (event) => {
+document.querySelector("#OCsButton")
+		.addEventListener("click", (event) => {
 	document.body.className = "OCs";
 })
-ArtButton.addEventListener("click", (event) => {
+document.querySelector("#ArtButton")
+		.addEventListener("click", (event) => {
 	document.body.className = "Art";
+})
+document.querySelector("#ProjectsButton")
+		.addEventListener("click", (event) => {
+	document.body.className = "Projects";
 })
 
 function minimiseOrClose(){
