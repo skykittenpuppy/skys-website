507c1c35796db7b22635bcc1f5e104eefb732001
Hannah Hadfield <robotooth13alternative@gmail.com>
2025-01-08 18:12:00 +0000

    added icon for datapacker's construct and fixed issue with marking projects WIP

--- Projects/script.js
+++ Projects/script.js
@@ -3,60 +3,71 @@ var projList = [
 		name: "MC Earth Pigs",
 		desc: "A Minecraft Data-Pack for 1.21.5 that adds 6 pig variants from Minecraft Earth",
 		updt: new Date("2025-01-08"),
+		tags: ["Datapack"],
 	},
 	{	func: addZip,
 		name: "Sky's Better Carpets",
 		desc: "A Minecraft Resource-Pack that makes carpets overhang onto the block below",
 		updt: new Date("2024-12-09"),
+		tags: ["Resourcepack"],
 	},
 	{	func: addZip,
 		name: "Sky's Better Discs",
 		desc: "A Minecraft Resource-Pack that retextures music discs to have an even-center",
 		updt: new Date("2024-09-18"),
+		tags: ["Resourcepack"],
 	},
 	{	func: addZip,
 		name: "Sky's Title Screen",
 		desc: "A Minecraft Resource-Pack that replaces the logo and background on the title screen",
 		updt: new Date("2024-12-11"),
+		tags: ["Resourcepack"],
 	},
 	{	func: addZip,
 		name: "Sky's Farmland Sides",
 		desc: "A Minecraft Resource-Pack that gives farmland blocks unique side textures",
 		updt: new Date("2024-09-18"),
+		tags: ["Resourcepack"],
 	},
 	{	func: addZip,
 		name: "Sky's Relit Flames",
 		desc: "A Minecraft Resource-Pack that revamps the looks of different flaming blocks, like torches",
 		updt: new Date("2024-09-27"),
+		tags: ["Resourcepack"],
 	},
 	{	func: addZip,
 		name: "Sky's Softer Cakes",
 		desc: "A Minecraft Resource-Pack that modifies the cake textures to make them look softer",
 		updt: new Date("2024-09-18"),
+		tags: ["Resourcepack"],
 	},
 	{
 		func: addLink,
-		name: "Datapacker's Construct [WIP]",
+		name: "Datapacker's Construct",
 		desc: "A Minecraft Data-Pack version of the Tinker's Construct Mod!",
 		link: "https://github.com/skykittenpuppy/Datapackers-Construct",
+		tags: ["Datapack", "WIP"],
 	},
 	{
 		func: addLink,
-		name: "PlateUp! MC [WIP]",
+		name: "PlateUp! MC",
 		desc: "A Minecraft Data-Pack that aims to recreate the game PlateUp!",
 		link: "https://github.com/skykittenpuppy/PlateUp-MC",
+		tags: ["Datapack", "WIP"],
 	},
 	{
 		func: addLink,
-		name: "Sky's Assorted Sweets [WIP]",
+		name: "Sky's Assorted Sweets",
 		desc: "A Minecraft Mod featuring Small to Medium tweaks",
 		link: "https://github.com/skykittenpuppy/Skys-Assorted-Sweets",
+		tags: ["Mod", "WIP"],
 	},
 	{
 		func: addLink,
-		name: "Community Life Series [Abandoned]",
+		name: "Community Life Series",
 		desc: "A Minecraft Data-Pack that aims to recreate the mechanics of the youtube Life Series SMP",
 		link: "https://github.com/skykittenpuppy/Community-Life-Series-Datapack",
+		tags: ["Datapack", "Abandoned"],
 	},
 ];
 
@@ -82,6 +93,11 @@ function addZip(container, info){
 	link.classList = "projectName directDownload"
 	container.appendChild(link);
 
+	let tags = document.createElement("b");
+	tags.innerText = info.tags[1] ?? "";
+	tags.classList = "projectTags"
+	container.appendChild(tags)
+
 	let desc = document.createElement("p");
 	desc.innerText = info.desc;
 	desc.classList = "projectDesc"
@@ -104,6 +120,11 @@ function addLink(container, info){
 	link.href = info.link;
 	link.classList = "projectName externalLink"
 	container.appendChild(link);
+
+	let tags = document.createElement("b");
+	tags.innerText = info.tags[1] ?? "";
+	tags.classList = "projectTags"
+	container.appendChild(tags)
 
 	let desc = document.createElement("p");
 	desc.innerText = info.desc;
