4231acdebf48274ad1c4799b6ba17378f29c4785
Hannah Hadfield <robotooth13alternative@gmail.com>
2025-02-01 22:07:25 +0000

    partial projects overhaul

--- Projects/script.js
+++ Projects/script.js
@@ -1,101 +1,91 @@
 var projList = [
-	{	func: addZip,
+	{	func: addLink,
 		name: "MC Earth Pigs",
-		description: "A Minecraft Data-Pack & Resource-Pack for 1.21.5 that adds 6 pig variants from Minecraft Earth \n (Put the zip both in your world's datapacks folder, as well as your resourcepacks folder)",
-		updated: new Date("2025-01-24"),
-		version: "v1.3",
-		tags: ["Datapack", "Resourcepack"],
+		description: "A Minecraft Data-Pack & Resource-Pack for 1.21.5 that adds 6 pig variants from Minecraft Earth",
+		link: "https://modrinth.com/datapack/mc-earth-pigs",
+		favicon: "CDN/Projects/modrinth.png",
 	},
-	{	func: addZip,
+	{	func: addLink,
 		name: "Cheaper Duplication",
 		description: "A Minecraft Data-Pack that adds/rebalances recipes for duplicating rare items.",
-		updated: new Date("2025-01-10"),
-		version: "v1.0",
-		tags: ["Datapack"],
+		link: "https://modrinth.com/datapack/cheaper-duplication",
+		favicon: "CDN/Projects/modrinth.png",
 	},
-	{	func: addZip,
+	{	func: addLink,
 		name: "Sky's Distinguished Variants",
 		description: "A Minecraft Resource-Pack that gives axolotl buckets, painting variants and (invisible item frames [WIP]) unique sprites.",
-		updated: new Date("2025-01-25"),
-		version: "beta-1",
-		tags: ["Resourcepack"],
+		link: "https://modrinth.com/resourcepack/skys-distinguished-variants",
+		favicon: "CDN/Projects/modrinth.png",
 	},
-	{	func: addZip,
+	{	func: addLink,
 		name: "Sky's Better Carpets",
 		description: "A Minecraft Resource-Pack that makes carpets overhang onto the block below",
-		updated: new Date("2024-12-09"),
-		version: "v1.1",
-		tags: ["Resourcepack"],
+		link: "https://modrinth.com/resourcepack/skys-better-carpets",
+		favicon: "CDN/Projects/modrinth.png",
 	},
-	{	func: addZip,
+	{	func: addLink,
 		name: "Sky's Better Discs",
 		description: "A Minecraft Resource-Pack that retextures music discs to have an even-center",
-		updated: new Date("2024-09-18"),
-		version: "v1.0",
-		tags: ["Resourcepack"],
+		link: "https://modrinth.com/resourcepack/skys-better-discs",
+		favicon: "CDN/Projects/modrinth.png",
 	},
-	{	func: addZip,
+	{	func: addLink,
 		name: "Sky's Farmland Sides",
 		description: "A Minecraft Resource-Pack that gives farmland blocks unique side textures",
-		updated: new Date("2024-09-18"),
-		version: "v1.0",
-		tags: ["Resourcepack"],
+		link: "https://modrinth.com/resourcepack/skys-farmland-sides",
+		favicon: "CDN/Projects/modrinth.png",
 	},
-	{	func: addZip,
+	{	func: addLink,
 		name: "Sky's Relit Flames",
 		description: "A Minecraft Resource-Pack that revamps the looks of different flaming blocks, like torches",
-		updated: new Date("2024-09-27"),
-		version: "v1.0",
-		tags: ["Resourcepack"],
+		link: "https://modrinth.com/resourcepack/skys-relit-flames",
+		favicon: "CDN/Projects/modrinth.png",
 	},
-	{	func: addZip,
+	{	func: addLink,
 		name: "Sky's Softer Cakes",
 		description: "A Minecraft Resource-Pack that modifies the cake textures to make them look softer",
-		updated: new Date("2024-09-18"),
-		version: "v1.0",
-		tags: ["Resourcepack"],
+		link: "https://modrinth.com/resourcepack/skys-softer-cakes",
+		favicon: "CDN/Projects/modrinth.png",
 	},
-	{	func: addZip,
+	{	func: addLink,
 		name: "Sky's Title Screen",
 		description: "A Minecraft Resource-Pack that replaces the logo and background on the title screen",
-		updated: new Date("2024-12-11"),
-		version: "v2.0",
-		tags: ["Resourcepack"],
+		link: "https://modrinth.com/resourcepack/skys-title-screen",
+		favicon: "CDN/Projects/modrinth.png",
 	},
-	{	func: addZip,
+	{	func: addLink,
 		name: "Sky's Unique Woods",
 		description: "A Minecraft Resource-Pack that replaces Dark Oak with Walnut and Pale Oak with Silver Maple, as well as darkening dark oak leaves' texture",
-		updated: new Date("2025-01-15"),
-		version: "v1.0",
-		tags: ["Resourcepack"],
+		link: "https://modrinth.com/resourcepack/skys-unique-woods",
+		favicon: "CDN/Projects/modrinth.png",
 	},
 	{
 		func: addLink,
 		name: "Datapacker's Construct",
 		description: "A Minecraft Data-Pack version of the Tinker's Construct Mod!",
 		link: "https://github.com/skykittenpuppy/Datapackers-Construct",
-		tags: ["Datapack", "", "WIP"],
+		favicon: "CDN/Projects/github.png",
 	},
 	{
 		func: addLink,
 		name: "PlateUp! MC",
 		description: "A Minecraft Data-Pack that aims to recreate the game PlateUp!",
 		link: "https://github.com/skykittenpuppy/PlateUp-MC",
-		tags: ["Datapack", "", "WIP"],
+		favicon: "CDN/Projects/github.png",
 	},
 	{
 		func: addLink,
 		name: "Sky's Assorted Sweets",
 		description: "A Minecraft Mod featuring Small to Medium tweaks",
 		link: "https://github.com/skykittenpuppy/Skys-Assorted-Sweets",
-		tags: ["Mod", "", "WIP"],
+		favicon: "CDN/Projects/github.png",
 	},
 	{
 		func: addLink,
 		name: "Community Life Series",
 		description: "A Minecraft Data-Pack that aims to recreate the mechanics of the youtube Life Series SMP",
 		link: "https://github.com/skykittenpuppy/Community-Life-Series-Datapack",
-		tags: ["Datapack", "", "Abandoned"],
+		favicon: "CDN/Projects/github.png",
 	},
 ];
 
@@ -109,34 +99,6 @@ for (let proj in projList) {
 	mainSection.appendChild(container);
 }
 
-function addZip(container, info){
-	let icon = document.createElement("img");
-	icon.src = "../CDN/Projects/"+info.name+".png";
-	icon.classList = "projectIcon"
-	container.appendChild(icon);
-
-	let link = document.createElement("a");
-	link.innerText = info.name;
-	link.href = "../CDN/Projects/"+info.name+".zip";
-	link.classList = "projectName directDownload"
-	container.appendChild(link);
-
-	let tags = document.createElement("b");
-	tags.innerText = info.tags[2] ?? "";
-	tags.classList = "projectTags"
-	container.appendChild(tags)
-
-	let desc = document.createElement("p");
-	desc.innerText = info.description;
-	desc.classList = "projectDesc"
-	container.appendChild(desc)
-
-	let other = document.createElement("p");
-	other.innerText = info.version+": "+info.updated.toDateString();
-	other.classList = "projectOther"
-	container.appendChild(other)
-}
-
 function addLink(container, info){
 	let icon = document.createElement("img");
 	icon.src = "../CDN/Projects/"+info.name+".png";
@@ -144,15 +106,18 @@ function addLink(container, info){
 	container.appendChild(icon);
 
 	let link = document.createElement("a");
-	link.innerText = info.name
+	link.classList = "projectName";
 	link.href = info.link;
-	link.classList = "projectName externalLink"
-	container.appendChild(link);
 
-	let tags = document.createElement("b");
-	tags.innerText = info.tags[2] ?? "";
-	tags.classList = "projectTags"
-	container.appendChild(tags)
+	let favicon = document.createElement("img");
+	favicon.src = info.favicon;
+	link.appendChild(favicon);
+
+	let name = document.createElement("p");
+	name.innerText = info.name
+	link.appendChild(name);
+
+	container.appendChild(link);
 
 	let desc = document.createElement("p");
 	desc.innerText = info.description;
