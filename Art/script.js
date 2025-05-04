90dfa3133796a01ae48669c639ec8b969941dd14
Hannah Hadfield <skykittenpuppy@beegirl.gay>
2025-05-04 01:18:41 +0100

    new render

--- Art/script.js
+++ Art/script.js
@@ -22,61 +22,84 @@ let nameHolder = document.createElement("span");
 nameHolder.className = "nameHolder";
 artSection.appendChild(nameHolder);
 
-var artList = [
+const Group = Object.freeze({
+	Main: "",
+	PS1: "Minecraft PS1",
+	Parody: "Parody",
+});
+
+const artList = [
 	{
 		file: "Tricky Trials.png",
 		name: "Tricky Trials",
-		group: "",
+		group: Group.Main,
+		date: new Date("12 June 2024"),
 	},
 	{
 		file: "Jungle Explorers.png",
 		name: "Jungle Explorers",
-		group: "",
+		group: Group.Main,
+		date: new Date("29 May 2024"),
 	},
 	{
 		file: "Desert Archeology.png",
 		name: "Desert Archeology",
-		group: "",
+		group: Group.Main,
+		date: new Date("23 May 2024"),
 	},
 	{
 		file: "Snowy Campsite.png",
 		name: "Snowy Campsite",
-		group: "",
+		group: Group.Main,
+		date: new Date("22 May 2024"),
 	},
 	{
 		file: "Lush Cave.png",
 		name: "The Lush Cave",
-		group: "",
+		group: Group.Main,
+		date: new Date("10 August 2022"),
 	},
 	{
 		file: "The Nether.png",
 		name: "The Nether",
-		group: "",
+		group: Group.Main,
+		date: new Date("10 August 2022"),
 	},
 	{
 		file: "Minecraft PS1 - Menu.mp4",
 		name: "Menu",
-		group: "Minecraft PS1",
+		group: Group.PS1,
+		date: new Date("06 February 2024"),
 	},
 	{
 		file: "Minecraft PS1 - Chest.mp4",
 		name: "Chest",
-		group: "Minecraft PS1",
+		group: Group.PS1,
+		date: new Date("08 February 2024"),
+	},
+	{
+		file: "Minecraft PS1 - Pale Garden.mp4",
+		name: "Pale Garden",
+		group: Group.PS1,
+		date: new Date("03 May 2025"),
 	},
 	{
 		file: "How Do I Craft This Again.png",
 		name: "How Do I Craft This Again?",
-		group: "Parody",
+		group: Group.Parody,
+		date: new Date("22 July 2023"),
 	},
 	{
 		file: "Screw The Nether.png",
 		name: "Screw The Nether!",
-		group: "Parody",
+		group: Group.Parody,
+		date: new Date("11 March 2023"),
 	},
 	{
 		file: "Moonquest.png",
 		name: "Moonquest",
-		group: "Parody",
+		group: Group.Parody,
+		date: new Date("20 March 2024"),
 	},
 ]
 
@@ -91,7 +114,10 @@ function swapTo(index) {
 	} else {
 		contentHolder.innerHTML = `<img src="CDN/Art/`+info.file+`"/>`;
 	}
-	nameHolder.innerText = info.group+"\n"+info.name;
+	nameHolder.innerText = 
+		(info.group != "" ? info.group+"\n" : "")
+		+info.name;
+		//+"\n"+info.date?.toDateString();
 }
 
 swapTo(selected);
